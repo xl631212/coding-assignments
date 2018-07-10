6e5d06719282aa90e46924d49682f341471c48f0
xuying li <xuyingli.kepler@gmail.com>
2018-07-10 13:32:40 -0700

    using flask to add query in SQL

---
+++ friends_create_read/server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import Flask, flash, render_template, request, redirect, session
+# import the function connectToMySQL from the file mysqlconnection.py
+from mysqlconnection import connectToMySQL
+app = Flask(__name__)
+# invoke the connectToMySQL function and pass it the name of the database we're using
+# connectToMySQL returns an instance of MySQLConnection, which we will store in the variable 'mysql'
+mysql = connectToMySQL('friendsdb')
+# now, we may invoke the query_db method
+print("all the users\n", mysql.query_db("SELECT * FROM friends;"))
+@app.route('/')
+def index():
+    all_friends = mysql.query_db("SELECT * FROM friends")
+    print("Fetched all friends", all_friends)
+    return render_template('index.html', friends = all_friends)
+
+@app.route('/create_friend', methods=['POST'])
+def create():
+    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(occupation)s, NOW(), NOW());"
+    data = {
+             'first_name': request.form['first_name'],
+             'last_name':  request.form['last_name'],
+             'occupation': request.form['occupation']
+           }
+    mysql.query_db(query, data)
+    return redirect('/')
+if __name__ == "__main__":
+	app.run(debug=True)
